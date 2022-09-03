ecbee315d75e2f11a274c13c4793bec30f06184b
Hammad <ch.hammad2914@gmail.com>
2022-09-03 09:43:06 +0500

    Java 1

---
+++ app.js
@@ -0,0 +1,272 @@
+// 1
+// alert ("Hello! Welcome to JavaScript.");
+// alert ("Error! Please enter a valid password.");
+// alert ("Welcome to Js land \n Happy Coding!");
+// alert ("Welcome to Js land...");
+// alert ("Happy Coding! \n Prevent this page from creating additional dialoges");
+
+
+
+// 2
+// var username =1
+// var myname = "Hammad Ali";
+// var message = "Hello World";
+// alert (message);
+// var name = "Jhone Doe";
+// var age = "15 years old";
+// var data = "Certified Mobile App Developer";
+// alert (name);
+// alert (age);
+// alert (data);
+// var pizza ="PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert (pizza);
+// var email = "ch.hammad2914@gmail.com";
+// alert ("My email address is " + email);
+// var book = "A smarter way to learn JavaScript.";
+// alert ("I am trying to learn from the book " + book);
+// document.write ("Yah! I can write HTML content through JavaScript.");
+// var design ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert (design);
+
+
+// 3
+// var age = " I am 20 years old";
+// alert (age);
+// var visits = "You have visited this site 6 times";
+// alert (visits);
+// var birthyear = 2002;
+// document.write ("My Birth year is " + birthyear);
+// document.write (". \n Data type of my declared variable is number");
+// var name = "John Doe";
+// var product = "T-Shirt";
+// var quantity = 5 ;
+// document.write ( name + " ordered " + quantity + product + "(s)" + " from XYZ clothing store");
+
+
+// 4
+// var w = 3;
+// var x = 4;
+// var y = 6;
+// var z = w + x + y ;
+// alert (z);
+
+// legal
+// var x ="ali";
+// var h1 = "ali";
+// var _s = "ali";
+// var $ = "ali";
+// var name = "ali";
+
+// // illegal
+// var 1a ="name";
+// var z-1 ="name";
+// var @ ="name";
+// var 23 ="name";
+// var 3$ ="name";
+// var a = "For example $my_1stVariable";
+// var b = "$name, _name or name";
+// var c = "Case sensitive ";
+// var d = "Keywords";
+// document.write ( "Rules for naming JS variables" + "<br>" + "<br>" + "Variable names can only contain Letters, numbers, $ or _ ." + a + "<br>" + "Variables must begin with a Letter, $ or _ ." + b + "<br>" + "Variable names are case " + c + "<br>" + "Variable names should not be JS " + d);
+
+
+// 5
+// var a = 3;
+// var b = 5;
+// var z = a + b;
+// document.write ("Sum of " + a + " and " + b + " is " + z);
+
+// var a = 3;
+// var b = 5;
+// var z = a - b;
+// document.write ("Abstract of " + a + " and " + b + " is " + z);
+
+// var a = 3;
+// var b = 5;
+// var z = a * b;
+// document.write ("Product of " + a + " and " + b + " is " + z);
+
+// var a = 3;
+// var b = 5;
+// var z = a / b;
+// document.write ("Dividend of " + a + " and " + b + " is " + z);
+
+// var a = 3;
+// var b = 5;
+// var z = a % b;
+// document.write ("Modulus of " + a + " and " + b + " is " + z);
+
+// var a1 =  5;
+// document.write ("The value after variable declaration is: ." + "<br>"  + " Initial value : " + a1);
+// a1 ++
+// document.write ("<br>" + "Value after increament : " + a1);
+// var a1 = a1 + 7
+// document.write ("<br>" + "Value after addition : " + a1);
+// a1 --
+// document.write ("<br>" + "Value after decrement : " + a1);
+// var a1 = a1 % 3;
+// document.write ("<br>" + "The remainder is : " + a1);
+
+// var ticket = 600 * 5;
+// document.write ("Total cost to buy 5 tickets to moive is " + ticket + "PKR");
+
+// var table = 6;
+// document.write ("Table of 6:" + "<br>" + "6x1= " + table + "<br>" +  "6x2= " + table * 2 + "<br>" +  "6x3= " + table * 3 + "<br>" +  "6x4= " + table * 4 + "<br>"+  "6x5= " + table * 5 + "<br>"+  "6x6= " + table * 6 + "<br>"+  "6x7= " + table * 7 + "<br>"+  "6x8= " + table * 8 + "<br>"+  "6x9= " + table * 9 + "<br>" +  "6x10= " + table * 10 );
+
+// var c = 20;
+// var c1 = (c * 9 / 5) + 32;
+// document.write (c + "C is " + c1 + "F" );
+
+// var f = 80;
+// var f1 = (f - 32) * 5 / 9;
+// document.write ("<br>" + f + "F is " + f1) + "C";
+
+// var item1 = 700;
+// var quan1 = 3;
+// var item2 = 230;
+// var quan2 = 6;
+// var ship = 130;
+// var total = (item1 * quan1) + (item2 * quan2) + ship
+// document.write ("Price of item 1 is " + item1 + "<br>" + "Quantity of item 1 is " + quan1 + "<br>" + "Price of item 2 is " + item2 + "<br>" + "Quantity of item 2 is " + quan2 + "<br>" + "Shipping Charges " + ship + "<br>" + "<br>" + "Total cost is " + total);
+
+// var tmarks = 1000;
+// var omarks = 874;
+// var per = omarks / tmarks * 100;
+// document.write ("MARKS SHEET" + "<br>" + "<br>" + "<br>" + "Total marks: " + tmarks + "<br>" + "Marks obtained : " + omarks + "<br>" + "Percentage: " + per);
+
+// var curren = (25*28) + ( 10 * 104.80);
+// document.write ("CURRENCY IN PKR" + "<br>" + "<br>" + "<br>" + "Total Currency in PKR: " + curren);
+
+// var num = 8 + 5 * 10 / 2;
+// alert (num);
+
+// var c = 2022;
+// var b = 2002;
+// var a = c - b;
+// document.write ("AGE CALCULATOR" +"<br>" + "<br>" +"Current Year: " + c + "<br>" + "Birth Year: " + b + "<br>" + "Your Age is: " + a);
+
+// var s = "Lays";
+// var age = 20;
+// var mage = 65;
+// var s1 = 3;
+// var tolal = (mage - age) * 3;
+// document.write ("THE LIFETIME SUPPLY CALCULATOR:" +"<br>" +"<br>" + "Favourate Snack: " + s +"<br>" + "Currenr Age: " + age +"<br>" + "Max Age: " + mage +"<br>" + "Snavks a day: " + s1 + "<br>" + "You will need " + tolal +  s + " until rip age.");
+
+// 6-9
+// var a = 10;
+// document.write ("The value of a is:" + a + "<br>" + "...........................");
+// var a = ++a;
+// document.write ("<br>" + "The value of a is:" + a + "<br>" + " Now the value of a is:" + a );
+// document.write ("<br>" + "The value of a is:" + a++ + "<br>" + " Now the value of a is:" + a);
+// var a = --a;
+// document.write ("<br>" + "The value of a is:" + a + "<br>" + " Now the value of a is:" + a);
+// document.write ("<br>" + "The value of a is:" + a-- + "<br>" + " Now the value of a is:" + a);
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// var r = --a - --b + ++b + b--;
+// document.write ("a is " + a +"<br>" + "b is " + b + "<br>" + "result is " + r);
+
+// var name = prompt("Your Name:");
+// var a = "Thanks for visiting";
+// alert (name);
+// alert (a);
+
+
+// // 9-10
+// var city = prompt("Enter City");
+// if (city === "karachi") { 
+//     alert ("Welcome to city of lights");
+// }
+
+// var gender = prompt("Enter your Gender");
+// if (gender === "male") {
+//     alert ("Good Morning Sir");
+// }
+// else if (gender === "female") { 
+//     alert ("Good Morning Ma'am.")
+// }
+
+// var fuel = prompt ("Enter reaming fuel(in litres)");
+// if (fuel < "0.25 litres") {
+//     alert ("Please refill fuel");
+// }
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");}
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// var num = 8;
+// var num1 = +prompt("Guess the number (1-10)");
+// if (num1 === num) {
+//     alert ("Bingo! Correct answer");
+// }
+//  if (num1 === num + 1) {
+//     alert ("Close enough to the correct answer.")
+// }
+
+// var num = prompt ("enter number divisible to 3 ");
+// if (num % 3 ===0) {
+//     alert ( "Number is divible to 3");
+// }
+
+// var num = prompt("Enter number");
+// if (num % 2 === 0) {
+//     alert ("Number is even")
+// }
+// else alert ("Number is odd");
+
+// var temp = prompt("Enter Temperature");
+// if (temp > 40 ) {
+//     alert ("“It is too hot outside.”")
+// }
+// else if (temp > 30) {
+//     alert ("“The Weather today is Normal.”")
+// }
+// else if (temp > 20 ) {
+//     alert ("“Today’s Weather is cool.”")
+// }
+// else if (temp > 10) {
+//     alert ("“OMG! Today’s weather is so Cool.”")
+// }
